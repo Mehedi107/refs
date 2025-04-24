8400804fd3d8c71bdfceccd4b08d1895ac98a112
Mehedi Hasan <mehedi.hsn.dev.@gmail.com>
2025-04-24 11:02:59 +0600

    feat: save user name logic in UI added

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,10 +1,21 @@
+import { useRef } from 'react';
+import { useState } from 'react';
+
 export default function Player() {
+  const inputVal = useRef();
+  const [playerName, setPlayerName] = useState(null);
+
+  function handleClick() {
+    setPlayerName(inputVal.current.value);
+    inputVal.current.value = '';
+  }
+
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      <h2>Welcome {playerName ? playerName : 'unknown'}</h2>
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input ref={inputVal} type="text" />
+        <button onClick={handleClick}>Set Name</button>
       </p>
     </section>
   );
