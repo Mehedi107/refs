68fc44aca0c7c333afd2cf4ef3c7945162142d73
Mehedi Hasan <mehedi.hsn.dev.@gmail.com>
2025-04-24 18:13:17 +0600

    implement timer logic

--- src/components/TimerChallenge.jsx
+++ src/components/TimerChallenge.jsx
@@ -1,14 +1,35 @@
+import { useRef, useState } from 'react';
+
 const TimerChallenge = ({ title, targetTime }) => {
+  const timer = useRef();
+  const [timeStart, setTimeStart] = useState(false);
+  const [timeExpired, setTimeExpired] = useState(false);
+
+  function handleStart() {
+    timer.current = setTimeout(() => {
+      setTimeExpired(true);
+    }, targetTime * 1000);
+
+    setTimeStart(true);
+  }
+
+  function handleStop() {
+    clearTimeout(timer.current);
+  }
+
   return (
     <div className="challenge">
       <h2>{title}</h2>
+      {timeExpired && <p>You Lost</p>}
       <p className="challenge-time">
         {targetTime} sec{targetTime > 1 ? 's' : ''}
       </p>
       <div>
-        <button>Start Challenge</button>
+        <button onClick={timeStart ? handleStop : handleStart}>
+          {timeStart ? 'Stop' : 'Start'} Challenge
+        </button>
       </div>
-      <p>Time is running.../ Timer inactive</p>
+      <p>{timeStart ? 'Time is running...' : 'Timer inactive'}</p>
     </div>
   );
 };
